394b40d796215513fc68a317bc573bdf39a0cf66
Daphne <daphne.feresin@gmail.com>
2020-05-07 18:23:28 -0400

    function

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
 
-
+function runTheClock(){
 
 var date = new Date();
 let hr = date.getHours();
@@ -19,3 +19,8 @@ console.log(hr+" "+min+" "+sec)
 HOURHAND.style.transform = "rotate(" + hrPosition +"deg)";
 MINUTEHAND.style.transform = "rotate(" + minPosition +"deg)";
 SECONDHAND.style.transform = "rotate(" + secPosition +"deg)";
+
+}
+
+var interval = setInterval(runTheClock, 1000);
+
